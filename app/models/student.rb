3d276c22b03c16dc7f3d130dbd4aef075d4dd68f
acagirici <acagirici23@gmail.com>
2021-11-07 21:48:45 -0500

    This is a new commit for what I originally planned to be amended

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,6 @@
 class Student < ApplicationRecord
     belongs_to :user #optional: true
+    belongs_to :category
     has_many :notes
     has_many :users, through: :notes
     validates :first_name, :last_name, :birth_date, :grade_level, :career_goal, presence: true
