ccddf11202fe52167e002bf50ec3e4ee370a4329
acagirici <acagirici23@gmail.com>
2021-11-08 22:28:16 -0500

    route update

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
 
+  get '/logout' => 'sessions#destroy'
   delete '/logout' => 'sessions#destroy'
 
   get '/auth/:provider/callback' => 'sessions#omni_signin'
